b2527964dedf35fd9f5eeb61754714b86aa00339
Eric Jacobson <contact@efjacobson.com>
2016-06-08 14:51:56 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Eric Jacobson</title>
+    
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/includes-header.php";
+      include($path);
+    ?>
+    
+  </head>
+
+  <body>
+
+    <div class="wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12">
+          <div class="col-sm-4 col-sm-push-8 col-md-4 col-md-push-8">
+            <h1>Info</h1>
+          </div>
+          <div class="col-sm-8 col-sm-pull-4 col-md-8 col-md-pull-4">
+            <div class="section-header">
+              <i class="fa fa-user fa-3x"></i>
+              <h1>Profile</h1>
+            </div>
+            <p>Full-stack web developer with several years of experience engineering and maintaining consumer-oriented business web applications. Firm believer in open communication, agile adaptation to changing demands, and providing long-term solutions to real-world problems.</p>
+            <div class="section-header">
+              <i class="fa fa-cog fa-3x"></i>
+              <h1>Skills</h1>
+            </div>
+            <p>Elastic, Transact-SQL, SQL Server Management Studio, C#, Visual Studio 2015, Team Foundation Server (TFS), MVC 5, MVC Attribute Routing, WebApi 2.0, AJAX, JSON Serialization, JSON Lint, jQuery, AngularJS, Bootstrap 3.0, CSS3, HTML5, Amazon Web Services (AWS S3) Google Maps API, LAMP, PHP, Node.js, NPM, GruntJS, SASS, Google AdWords, Google Analytics and Google Webmaster Tools.</p>
+            <div class="section-header">
+              <i class="fa fa-user fa-3x"></i>
+              <h1>Experience</h1>
+            </div>
+            <div class="section-subhead">
+              <h4 class="job-title">WEB DEVELOPER</h4>
+              <h4 class="job-duration">2016 - Present</h4>
+            </div>
+            <h5 class="job-location">HealSwift, Los Angeles CA</h5>
+            <p>Worked as a full-stack developer for HealSwift (<a href="https://healswift.azurewebsites.net/">healswift.azurewebsites.net</a>), a healthcare startup dedicated to matching patients with doctors using a proprietary multi-parameter seach algorithm. Patients can manage providers and hospitals, submit reviews, and contact doctors by phone, email, and SMS. Providers can verify their profiles publish additional information not found in our data-gathering process.</p>
+            <p>HealSwift is a Hybrid SPA/ASP.NET MVC web application backed by Microsoft SQL Server and Elastic, exposed through a RESTful Web API. The web client leverages AngularJS and responsive design for desktop, tablets, and mobile devices.</p>
+            <p>Employed an agile development process with daily standups and Trello for task management.</p>
+            <p>Technologies include Elastic, Transact-SQL, SQL Server Management Studio, C#, Visual Studio 2015, Team Foundation Server (TFS), MVC 5, MVC Attribute Routing, WebApi 2.0, AJAX, JSON Serialization, JSON Lint, jQuery, AngularJS, Bootstrap 3.0, CSS3, HTML5, Amazon Web Services (AWS S3) and Google Maps API.</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $path .= "/inc/includes-footer.php";
+      include($path);
+    ?>
+  
+  </body>
+</html>
+
+
